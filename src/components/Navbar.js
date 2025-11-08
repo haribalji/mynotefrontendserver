81ab93bd807fcc7deddae32e1cb775844264296a
hari balaji <145361290+haribalji@users.noreply.github.com>
2025-11-08 18:31:16 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -92,25 +92,25 @@ const Navbar = () => {
             SmartSummarizer
           </Link>
 
-          <Link
-            className={`nav-link ${
-              location.pathname === "/upload" ? "active" : ""
-            }`}
-            to="/upload"
-            onClick={handleNavCollapse}
-          >
-            Image learning
-          </Link>
+          // <Link
+          //   className={`nav-link ${
+          //     location.pathname === "/upload" ? "active" : ""
+          //   }`}
+          //   to="/upload"
+          //   onClick={handleNavCollapse}
+          // >
+          //   Image learning
+          // </Link>
 
-          <Link
-            className={`nav-link ${
-              location.pathname === "/pdfchat" ? "active" : ""
-            }`}
-            to="/pdfchat"
-            onClick={handleNavCollapse}
-          >
-            Chat with PDF
-          </Link>
+          // <Link
+          //   className={`nav-link ${
+          //     location.pathname === "/pdfchat" ? "active" : ""
+          //   }`}
+          //   to="/pdfchat"
+          //   onClick={handleNavCollapse}
+          // >
+          //   Chat with PDF
+          // </Link>
 
           <Link
             className={`nav-link ${
